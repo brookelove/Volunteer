fb0053346cd31cb18762dbda1821aa85c9c8102a
Brooke Love <savienloveb@gmail.com>
2022-05-13 20:00:16 -0700

    installing packages, creating sequalize connection

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+const Seqelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequalixe = new Seqelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Seqelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host:'localhost',
+            dialect: 'mysql',
+            port: 3306
+        }
+    );
+}
+
+module.exports = sequelize
